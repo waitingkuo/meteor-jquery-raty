50dcc6fb399406d20549263456d19301ef0fbaea
waitingkuo <waitingkuo0527@gmail.com>
2014-07-21 15:26:51 +0800

    created jquery-raty package

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-meteor-jquery-raty
-==================
+# Raty - A jQuery star Rating Plugin
 
-Raty - A jQuery star Rating Plugin
+[Raty](http://wbotelhos.com/raty) v.2.7.0 is a easy to use jQuery star plugin for star rating system
+
+
+## How to install
+1. `npm install -g meteorite`
+2. `mrt add jquery-raty`
+
+
+## Simple Usage
+
+You need just to have a div to build the Raty
+
+HTML:
+
+    <div class="raty"></div>
+
+JS:
+    $('.raty').raty();
+
+
+## Documentation
+
+[http://wbotelhos.com/raty](http://wbotelhos.com/raty)
---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  summery: 'Raty - A jQuery star Rating Plugin'
+})
+
+Package.on_use(function (api) {
+
+  api.use('jquery', 'client');
+  api.imply('jquery', 'client');
+  api.add_files([
+    'lib/fonts/raty.eot',
+    'lib/fonts/raty.svg',
+    'lib/fonts/raty.ttf',
+    'lib/fonts/raty.woff',
+    'lib/images/cancel-off.png',
+    'lib/images/cancel-on.png',
+    'lib/images/star-half.png',
+    'lib/images/star-off.png',
+    'lib/images/star-on.png',
+    'lib/jquery.raty.css',
+    'lib/jquery.raty.js'
+  ], 
+    'client'
+  )
+
+});
