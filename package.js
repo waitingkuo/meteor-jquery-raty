08221f9762de6c83fa19a829cb5bb17310c766c3
Vladimir Momirov <vladimir.momirov@saturized.com>
2015-01-29 01:07:55 +0100

    Use raty lib from submodule, it is easier to maintain.

--- package.js
+++ package.js
@@ -7,18 +7,18 @@ Package.on_use(function (api) {
   api.use('jquery', 'client');
   api.imply('jquery', 'client');
   api.add_files([
-    'lib/fonts/raty.eot',
-    'lib/fonts/raty.svg',
-    'lib/fonts/raty.ttf',
-    'lib/fonts/raty.woff',
-    'lib/images/cancel-off.png',
-    'lib/images/cancel-on.png',
-    'lib/images/star-half.png',
-    'lib/images/star-off.png',
-    'lib/images/star-on.png',
-    'lib/jquery.raty.css',
-    'lib/jquery.raty.js'
-  ], 
+    'raty/lib/fonts/raty.eot',
+    'raty/lib/fonts/raty.svg',
+    'raty/lib/fonts/raty.ttf',
+    'raty/lib/fonts/raty.woff',
+    'raty/lib/images/cancel-off.png',
+    'raty/lib/images/cancel-on.png',
+    'raty/lib/images/star-half.png',
+    'raty/lib/images/star-off.png',
+    'raty/lib/images/star-on.png',
+    'raty/lib/jquery.raty.css',
+    'raty/lib/jquery.raty.js'
+  ],
     'client'
   )
 
